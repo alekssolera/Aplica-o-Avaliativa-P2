446b1ca7e9fbcf379b5ee72b1763243ea56ea8a5
Alex Nascimento Solera <nalex8130@gmail.com>
2025-06-08 19:22:46 -0300

    Elementos da Tela de Login

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,11 +30,14 @@
         {
             textBox1 = new TextBox();
             label1 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            button1 = new Button();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(12, 39);
+            textBox1.Location = new Point(297, 80);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(149, 23);
             textBox1.TabIndex = 0;
@@ -42,17 +45,45 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 21);
+            label1.Location = new Point(297, 62);
             label1.Name = "label1";
             label1.Size = new Size(50, 15);
             label1.TabIndex = 1;
             label1.Text = "Usuário:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(297, 121);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Senha:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(297, 139);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(149, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(297, 184);
+            button1.Name = "button1";
+            button1.Size = new Size(149, 34);
+            button1.TabIndex = 4;
+            button1.Text = "Logar";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(textBox1);
             Name = "Form1";
@@ -65,5 +96,8 @@
 
         private TextBox textBox1;
         private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private Button button1;
     }
 }
