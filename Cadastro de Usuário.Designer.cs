0bb8a289af908d13e1495233bebf7fc4fee0dbc0
Alex Nascimento Solera <nalex8130@gmail.com>
2025-06-08 20:34:59 -0300

    Criação da Interface do Formulário de Cadastro de Usuário

--- Cadastro de Usuário.Designer.cs
+++ Cadastro de Usuário.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Cadastro_de_Usuário";
+            btnCadastrar = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // btnCadastrar
+            // 
+            btnCadastrar.Location = new Point(381, 164);
+            btnCadastrar.Name = "btnCadastrar";
+            btnCadastrar.Size = new Size(200, 40);
+            btnCadastrar.TabIndex = 0;
+            btnCadastrar.Text = "Cadastrar";
+            btnCadastrar.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(381, 44);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(200, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(381, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Usuário:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(381, 104);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(200, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(381, 86);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Senha:";
+            // 
+            // Cadastro_de_Usuário
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(997, 450);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(btnCadastrar);
+            Name = "Cadastro_de_Usuário";
+            Text = "Cadastro_de_Usuário";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnCadastrar;
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox2;
+        private Label label2;
     }
 }
