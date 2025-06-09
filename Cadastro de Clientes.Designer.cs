5c677a876db82292d4ec1336285975366211cc9c
Alex Nascimento Solera <nalex8130@gmail.com>
2025-06-09 11:51:42 -0300

    Inserção dos Elementos do Cadastro de Cliente

--- Cadastro de Clientes.Designer.cs
+++ Cadastro de Clientes.Designer.cs
@@ -28,12 +28,274 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Cadastro_de_Clientes";
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            btnCadastrarCliente = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 27);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(163, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 70);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(163, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 112);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(163, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(12, 153);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(163, 23);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(12, 197);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(163, 23);
+            textBox5.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 52);
+            label2.Name = "label2";
+            label2.Size = new Size(28, 15);
+            label2.TabIndex = 6;
+            label2.Text = "CPF";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 94);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Email:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 135);
+            label4.Name = "label4";
+            label4.Size = new Size(54, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Telefone:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 179);
+            label5.Name = "label5";
+            label5.Size = new Size(62, 15);
+            label5.TabIndex = 9;
+            label5.Text = "WhatsApp";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(243, 27);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 10;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(243, 70);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(100, 23);
+            textBox7.TabIndex = 11;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(243, 112);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(100, 23);
+            textBox8.TabIndex = 12;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(371, 27);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(100, 23);
+            textBox9.TabIndex = 13;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(371, 70);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(100, 23);
+            textBox10.TabIndex = 14;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(371, 112);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(100, 23);
+            textBox11.TabIndex = 15;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(243, 9);
+            label6.Name = "label6";
+            label6.Size = new Size(28, 15);
+            label6.TabIndex = 16;
+            label6.Text = "CEP";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(243, 53);
+            label7.Name = "label7";
+            label7.Size = new Size(69, 15);
+            label7.TabIndex = 17;
+            label7.Text = "Logradouro";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(243, 96);
+            label8.Name = "label8";
+            label8.Size = new Size(51, 15);
+            label8.TabIndex = 18;
+            label8.Text = "Número";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(371, 9);
+            label9.Name = "label9";
+            label9.Size = new Size(38, 15);
+            label9.TabIndex = 19;
+            label9.Text = "Bairro";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(371, 53);
+            label10.Name = "label10";
+            label10.Size = new Size(44, 15);
+            label10.TabIndex = 20;
+            label10.Text = "Cidade";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(371, 96);
+            label11.Name = "label11";
+            label11.Size = new Size(42, 15);
+            label11.TabIndex = 21;
+            label11.Text = "Estado";
+            // 
+            // btnCadastrarCliente
+            // 
+            btnCadastrarCliente.Location = new Point(12, 256);
+            btnCadastrarCliente.Name = "btnCadastrarCliente";
+            btnCadastrarCliente.Size = new Size(163, 65);
+            btnCadastrarCliente.TabIndex = 22;
+            btnCadastrarCliente.Text = "Cadastrar";
+            btnCadastrarCliente.UseVisualStyleBackColor = true;
+            // 
+            // Cadastro_de_Clientes
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnCadastrarCliente);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(textBox11);
+            Controls.Add(textBox10);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Cadastro_de_Clientes";
+            Text = "Cadastro_de_Clientes";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox11;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Button btnCadastrarCliente;
     }
 }
