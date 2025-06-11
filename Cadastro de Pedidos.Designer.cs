78817ccd223e4da3f8b558a352f9e66f04daf7ee
Alex Nascimento Solera <nalex8130@gmail.com>
2025-06-10 22:10:35 -0300

    Inserção dos componentes da formulário de cadastro de pedidos

--- Cadastro de Pedidos.Designer.cs
+++ Cadastro de Pedidos.Designer.cs
@@ -28,12 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Cadastro_de_Pedidos";
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            BtnBuscaCpf = new Button();
+            listBox1 = new ListBox();
+            comboBox1 = new ComboBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 34);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(116, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 15);
+            label1.TabIndex = 1;
+            label1.Text = "CPF Cliente:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(180, 16);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Cliente:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(180, 34);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(116, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // BtnBuscaCpf
+            // 
+            BtnBuscaCpf.Location = new Point(31, 63);
+            BtnBuscaCpf.Name = "BtnBuscaCpf";
+            BtnBuscaCpf.Size = new Size(75, 23);
+            BtnBuscaCpf.TabIndex = 4;
+            BtnBuscaCpf.Text = "Buscar CPF";
+            BtnBuscaCpf.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(12, 210);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(776, 139);
+            listBox1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(12, 163);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(154, 23);
+            comboBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 145);
+            label3.Name = "label3";
+            label3.Size = new Size(128, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Selecione Os Produtos:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(209, 163);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(122, 23);
+            textBox3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(209, 145);
+            label4.Name = "label4";
+            label4.Size = new Size(72, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Quantidade:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(362, 162);
+            button1.Name = "button1";
+            button1.Size = new Size(184, 23);
+            button1.TabIndex = 10;
+            button1.Text = "Adicionar ao Pedido";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(649, 355);
+            button2.Name = "button2";
+            button2.Size = new Size(139, 42);
+            button2.TabIndex = 11;
+            button2.Text = "Finalizar Pedido";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Cadastro_de_Pedidos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(comboBox1);
+            Controls.Add(listBox1);
+            Controls.Add(BtnBuscaCpf);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Cadastro_de_Pedidos";
+            Text = "Cadastro_de_Pedidos";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private Button BtnBuscaCpf;
+        private ListBox listBox1;
+        private ComboBox comboBox1;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private Button button1;
+        private Button button2;
     }
 }
